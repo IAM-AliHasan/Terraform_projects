5cc297ffdde797720b4c9c3b396385e316e64ceb
IAM-AliHasan <ah7069249@gmail.com>
2024-07-30 13:01:55 +0500

    this is the third version

--- main.tf
+++ main.tf
@@ -1,28 +1,4 @@
-<<<<<<< HEAD
-variable "ami_value" {
-  description = " value for the ami"
-  default = "ami-0bb84b8ffd87024d8"
-}
-variable "instance_type_value" {
-    description = "value for the instance type"
-    default = "t2.micro"
-  
-}
 
-
-provider "aws" {
-  region = "us-east-1"
-}
-resource "aws_instance" "example" {
-  ami = var.ami_value
-  instance_type= var.instance_type_value
-
-}
-output "public-ip-address" {
-  value = aws_instance.example.public_ip
-}
-
-=======
 terraform {
   required_providers {
     aws = {
@@ -64,4 +40,4 @@ resource "aws_instance" "public_instance" {
     Name = "HelloWorld"
   }
 }
->>>>>>> 080ca2c (this is the first version of the file)
+
