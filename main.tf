dffed6849c50000cc0f4d24c7f72d7a44c248afc
IAM-AliHasan <ah7069249@gmail.com>
2024-07-30 12:05:30 +0500

    this is the second version

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 variable "ami_value" {
   description = " value for the ami"
   default = "ami-0bb84b8ffd87024d8"
@@ -21,3 +22,46 @@ output "public-ip-address" {
   value = aws_instance.example.public_ip
 }
 
+=======
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "tls_private_key" "rsa_4096" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+variable "key_name" {
+  
+}
+
+resource "aws_key_pair" "key_pair" {
+  key_name   = var.key_name
+  public_key = tls_private_key.rsa_4096.public_key_openssh
+}
+
+resource "local_file" "private_key" {
+  content = tls_private_key.rsa_4096.private_key_pem
+  filename = var.key_name
+  
+}
+resource "aws_instance" "public_instance" {
+    ami = "ami-04a81a99f5ec58529"
+    instance_type = "t2.micro"
+    key_name = aws_key_pair.key_pair.key_name
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+>>>>>>> 080ca2c (this is the first version of the file)
